bbd9d757fa68b09902d0228498884698218d7efb
Artur Mauricio <arturmauricioss@outlook.com>
2026-01-11 16:35:00 -0300

    adicionando Cors

--- projetos.py
+++ projetos.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Isso permite requisições de qualquer origem
 
 @app.route("/")
 def home():
@@ -9,7 +11,6 @@ def home():
 @app.route("/soma/<int:a>/<int:b>")
 def soma(a, b):
     return jsonify({"resultado": a + b})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
